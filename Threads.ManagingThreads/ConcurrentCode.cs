41d8fb1cc41063065d5da13be212b22abc2c5a03
pkaselj <petar.kaselj.00@gmail.com>
2023-10-19 23:54:03 +0200

    Exercise 3

---
+++ Threads.ManagingThreads/ConcurrentCode.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+
+namespace Threads.ManagingThreads
+{
+    public class ConcurrentCode
+    {
+        public volatile bool done = false;
+
+        public void SleepWhileCodeIsNotDone()
+        {
+            var managedThreadId = Environment.CurrentManagedThreadId;
+            while (!done)
+            {
+                Console.WriteLine($"SleepWhileCodeIsNotDone ({managedThreadId}): Sleeping for 2s");
+                Thread.Sleep(2000);
+            }
+
+            Console.WriteLine($"SleepWhileCodeIsNotDone ({managedThreadId}): Finished.");
+        }
+
+        public void SleepWhileCancellationIsNotRequested(CancellationToken cancellationToken)
+        {
+            int managedThreadId = Environment.CurrentManagedThreadId;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"SleepWhileCancellationIsNotRequested ({managedThreadId}): Sleeping for 1s");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"SleepWhileCancellationIsNotRequested ({managedThreadId}): Finished.");
+        }
+
+
+        public void DoSleepForLongTime()
+        {
+            int managedThreadId = Thread.CurrentThread.ManagedThreadId;
+
+            var iterations = 10000000;
+
+            Console.WriteLine($"DoSleepForLongTime ({managedThreadId}): Doing work for {iterations} iterations.");
+
+            Thread.SpinWait(iterations);
+
+            try
+            {
+                Console.WriteLine($"DoSleepForLongTime ({managedThreadId}): Going to sleep for very long time!");
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine($"DoSleepForLongTime ({managedThreadId}): Sleep interupted!");
+                Console.WriteLine($"DoSleepForLongTime ({managedThreadId}): Exception: {e.Message}");
+                Console.WriteLine($"DoSleepForLongTime ({managedThreadId}): Finished.");
+            }
+        }
+    }
+}
---
+++ Threads.ManagingThreads/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads.ManagingThreads
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int mid = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Primary Thread ({0}) ", mid);
+
+            var delay = TimeSpan.FromSeconds(10);
+            var cancellationTokeSource = new CancellationTokenSource(delay);
+
+            var code = new ConcurrentCode();
+            var threads = new[]
+            {
+                new Thread(new ThreadStart(code.SleepWhileCodeIsNotDone)),
+                new Thread(() => code.SleepWhileCancellationIsNotRequested(cancellationTokeSource.Token)),
+                new Thread(code.DoSleepForLongTime),
+            };
+
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Start();
+
+            Thread.Sleep(3000);
+
+            bool alive = threads[2].IsAlive;
+            if (alive)
+            {
+                Console.WriteLine($"Primary: Thread {{0}} is alive.", threads[2].ManagedThreadId);
+            }
+
+            code.done = true;
+            threads[2].Interrupt();
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Main: All threads have terminated.");
+
+            Console.ResetColor();
+        }
+    }
+}
